5397da72cf1c3a716b547d2e2cf45fa4d3e6e058
Hoelac0ste <Hoelacoste@gmail.com>
2024-10-30 10:21:50 +0300

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -1,12 +1,14 @@
+strings_positions = {}
 
-def custom_write(*strings, file_name = 'file_name.txt'):
-    strings_positions = {}
-    a = 1
+def custom_write(*strings, file_name = 'file1.txt'):
+    a = len(strings_positions) + 1
     for i in strings:
-        file = open(file_name, 'a')
+        file = open(file_name, 'a', encoding='utf-8')
         strings_positions[f'{a}, {file.tell()}'] = i
         file.write(f'{i}\n')
         file.close()
+        a += 1
     print(strings_positions)
 
-custom_write('hey', 'hello', 'hola')
+custom_write('hey', 'hello', 'hola', 'привет')
+custom_write('hey', 'hello', 'hola', 'привет')
